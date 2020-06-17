296385a6a73e4508c25b45881a6c7952aa748dd8
Yuki Koyama <yuki@koyama.xyz>
2020-06-17 19:28:37 +0900

    Add code to set contour

--- btoon/__init__.py
+++ btoon/__init__.py
@@ -15,6 +15,34 @@ bl_info = {
 }
 
 
+# ------------------------------------------------------------------------------
+# Copied from blender-cli-rendering:
+# https://github.com/yuki-koyama/blender-cli-rendering
+# ------------------------------------------------------------------------------
+# https://docs.blender.org/api/current/bpy.types.SolidifyModifier.html
+def add_solidify_modifier(mesh_object: bpy.types.Object,
+                          thickness: float = 0.01,
+                          flip_normal: bool = False,
+                          fill_rim: bool = True,
+                          material_index_offset: int = 0,
+                          shell_vertex_group: str = "",
+                          rim_vertex_group: str = "") -> None:
+
+    modifier: bpy.types.SolidifyModifier = mesh_object.modifiers.new(name="Solidify", type='SOLIDIFY')
+
+    modifier.material_offset = material_index_offset
+    modifier.thickness = thickness
+    modifier.use_flip_normals = flip_normal
+    modifier.use_rim = fill_rim
+
+    # TODO: Check whether shell_vertex_group is either empty or defined
+    # TODO: Check whether rim_vertex_group is either empty or defined
+
+    modifier.shell_vertex_group = shell_vertex_group
+    modifier.rim_vertex_group = rim_vertex_group
+# ------------------------------------------------------------------------------
+
+
 class BTOON_OP_SetContour(bpy.types.Operator):
 
     bl_idname = "node.set_contour"
@@ -23,7 +51,10 @@ class BTOON_OP_SetContour(bpy.types.Operator):
     bl_options = {"REGISTER", "UNDO"}
 
     def execute(self, context: bpy.types.Context):
-        self.report({'INFO'}, "TODO.")
+
+        add_solidify_modifier(bpy.context.object, -0.01, True, False, 1, "Contour", "")
+
+        self.report({'INFO'}, "Set contour.")
         return {'FINISHED'}
 
 
